624a0ddafda4e920fa126bdde5ca96f7e83e4798
vlabUser <userEmail>
2024-03-14 19:25:20 +0530

    Commiting Extension

--- main.py
+++ main.py
@@ -4,27 +4,29 @@
 '''
 import math
 
-# import the classes
+from complex import Complex
 
+from polar import Polar
 
 def modulus(c:Complex):
-    '''return modulus of the complex number'''
+    return (c.x**2+c.y**2)**0.5
     
 
 def arg(c:Complex):
-    '''return arg (angle) of the complex number'''
-    
+    return math.atan2(c.y, c.x)
 
 def abscissa(p:Polar):
     '''return abscissa of the polar point'''
+    return p.r*math.cos(p.t)
     
 
 def ordinate(p:Polar):
     '''return ordinate of the polar point'''
-    
+    return p.r*math.sin(p.t)
 
 def distance(z1:Complex, z2:Complex):
     '''distance between points'''
+    return ((z1.x-z2.x)**2+(z1.y-z2.y)**2)**0.5
     
 
 if __name__ == '__main__':
@@ -39,3 +41,4 @@ if __name__ == '__main__':
     # print(modulus(z)) # you can call after you implement
     x = Polar(2,math.pi/3) # uses the overloaded power
     print(x ** 2)
+    print(modulus(a))
